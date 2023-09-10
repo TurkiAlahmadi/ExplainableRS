8554c1f0f1d9d31b7e3dd95ea5422068512811d2
Turki Alahmadi <turki.369@gmail.com>
2023-09-10 16:56:41 +0200

    implement introduction steps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,119 @@
 import './App.css';
 import { UserProfile } from "./components/UserProfile";
+import React, {useState, useRef} from "react";
+import { Steps } from 'intro.js-react';
+import "intro.js/introjs.css";
 
 function App() {
 
-  return (
-    <div className='main-page'>
-        <p id="app-header">MFExplain</p>
-        <UserProfile/>
-    </div>
-  );
+    // Define state variables
+    const [initialStepsEnabled, setInitialStepsEnabled] = useState(true);
+    const [showInitialSteps, setShowInitialSteps] = useState(true);
+    const stepsRef = useRef(null);
+
+    // Define intro steps
+    const initialSteps = [
+        {
+            element: '.UserProfile',
+            intro: 'In this section, you can create and edit your movie profile.',
+            position: 'middle-right',
+        },
+        {
+            element: '.movie-select',
+            intro: 'Select a movie title from the dropdown menu to add it to your profile. ' +
+                'You can search for a specific movie title or type a release year to filter the ' +
+                'options and find a movie to rate.',
+            position: 'right',
+        },
+        {
+            element: '.dropdown-menus-container',
+            intro: 'Rate the selected movie on a scale from 0.5 to 5.0',
+            position: 'right',
+        },
+        {
+            element: '#addMovieButton',
+            intro: 'Add the movie to your movie profile',
+            position: 'right',
+        },
+        {
+            element: '.group-container',
+            intro: 'Your rated movies appear here. ' +
+                'You can edit your rating by clicking on the stars and ' +
+                'remove the movie from your profile by clicking on the delete ' +
+                'button to the right',
+            position: 'right',
+        },
+        {
+            element: '#submitButton',
+            intro: 'After adding at least five movies to your movie profile, ' +
+                'click on Generate Recommendations button to get movie recommendations.',
+            position: 'right',
+        },
+        {
+            element: '.movie-select',
+            intro: 'Now, add more movies to your movie profile.',
+            position: 'right',
+        },
+    ];
+
+    return (
+        <div className='main-page'>
+            <p id="app-header">MFExplain</p>
+            <UserProfile/>
+            <Steps
+                enabled={showInitialSteps}
+                steps={initialSteps}
+                initialStep={0}
+                onExit={() => setInitialStepsEnabled(false)}
+                onComplete={() => setShowInitialSteps(false)}
+                tooltipClass="custom-tooltip"
+                options={{
+                    tooltipClass: "custom-tooltip",
+                    tooltipOptions: {
+                        placement: "top",
+                        arrowOffset: 20,
+                        showStepNumbers: true,
+                        showButtons: true,
+                    },
+                }}
+            />
+            <style>
+                {`.custom-tooltip {
+                     background-color: #faf9f9;
+                     color: #555b6e;
+                     border-radius: 20px;
+                     font-size: 16px;    
+                }
+                .custom-tooltip .introjs-button {
+                      height: 20px;
+                      width: 80px;
+                      padding: 0px 0px;
+                      border-radius: 30px;
+                      border-width: 1px;
+                      border-style: solid;
+                      border-color: #555b6e;
+                      background-color: #555b6e;
+                      color: #faf9f9;
+                      font-weight: bold;
+                      font-size: 11px;
+                      text-align: center;
+                      text-shadow: none;  
+                }
+                .custom-tooltip .introjs-skipbutton {
+                      padding: 0px 0px;
+                      position: absolute;
+                      top: 0px;
+                      right: 0px;
+                      border-radius: 60px;
+                      background-color: #faf9f9;
+                      color: #555b6e;
+                      font-weight: bold;
+                      font-size: 14px;  
+                `
+                }
+            </style>
+        </div>
+    );
 };
 
 export default App;
--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,34 +1,62 @@
-import {NewMovie} from "./NewMovie";
-import {MovieList} from "./MovieList";
-import {Figures} from "./Figures";
-import {InitialPage} from "./InitialPage";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { MovieList } from "./MovieList";
+import { NewMovie } from "./NewMovie";
+import { Figures } from "./Figures";
+import { InitialPage } from "./InitialPage";
 import Button from "react-bootstrap/Button";
+import { Steps } from 'intro.js-react';
+import "intro.js/introjs.css";
 
 export const UserProfile = () => {
-    const [newMovie, setNewMovie] = useState({});
-    const [newRating, setNewRating] = useState(0);
-
     const [userData, setUserData] = useState([]);
     const [itemData, setItemData] = useState([]);
     const [userMovies, setUserMovies] = useState([]);
     const [movieUsers, setMovieUsers] = useState([]);
     const [recommendedItems, setRecommendedItems] = useState([]);
-
     const [movieTitles, setMovieTitles] = useState([]);
-    const [allMovies, setAllMovies] = useState([]);
 
+    const [newMovie, setNewMovie] = useState({});
+    const [newRating, setNewRating] = useState(0);
     const [selectedTitle, setSelectedTitle] = useState(null);
     const [selectedRating, setSelectedRating] = useState(null);
+    const [allMovies, setAllMovies] = useState([]);
 
     const [isLoadingData, setIsLoadingData] = useState(true);
     const [isLoadingPosters, setIsLoadingPosters] = useState(false);
     const [isGeneratingRecs, setIsGeneratingRecs] = useState(false);
+    const [showSteps, setShowSteps] = useState(true);
+    const [recsStepsEnabled, setRecsStepsEnabled] = useState(true);
 
+    // Use a ref to track the "Add Movie" button element
+    const addMovieButtonRef = useRef(null);
 
-
-    // Fetch movie titles from the Flask backend
     useEffect(() => {
+        // Ensure the ref is populated with the button element
+        addMovieButtonRef.current = document.getElementById('addMovieButton');
+    }, []);
+
+    // Define steps
+    const recsSteps = [
+        {
+            element: '.recommendations-posters',
+            intro: 'These are the your recommended movies either because of their ' +
+                'similarity to movies you liked or because similar users liked them. ',
+            position: 'right',
+        },
+        {
+            element: '.item-space',
+            intro: 'In this space, you can see all movies and interact with them.',
+            position: 'right',
+        },
+        {
+            element: '.user-space',
+            intro: 'In this space, you can see all users and interact with them.',
+            position: 'left',
+        },
+    ];
+
+    useEffect(() => {
+        // Fetch movie titles from the Flask backend
         const fetchMovieTitles = async () => {
             try {
                 const response = await fetch("http://localhost:5000/data");
@@ -43,7 +71,7 @@ export const UserProfile = () => {
         fetchMovieTitles();
     }, []);
 
-    const handleTitleChange = (selectedOption) => {
+    const handleTitleSelect = (selectedOption) => {
         setSelectedTitle(selectedOption);
         setNewMovie((prev) => ({
             ...prev,
@@ -51,7 +79,7 @@ export const UserProfile = () => {
         }));
     };
 
-    const handleRatingChange = (selectedOption) => {
+    const handleRatingSelect = (selectedOption) => {
         setSelectedRating(selectedOption);
         setNewMovie((prev) => ({
             ...prev,
@@ -63,64 +91,74 @@ export const UserProfile = () => {
         event.preventDefault();
         let terminate = false
 
+        if (Object.keys(newMovie).length === 0 || newMovie.rating == 0) {
+            alert("Please add a movie and rate it");
+        }
+
         // Check if the selected title is in the list of rated movie
         allMovies.forEach(movie => {
             if (movie.title === newMovie.title) {
-                alert("You already rated this movie! Delete it from the list to rate it again.");
+                alert("You already added this movie!");
                 terminate = true;
-            }
+            };
         });
+
         if (!terminate && newMovie.title && newMovie.rating) {
             // Assign a unique id to the new movie
-            const newMovieWithId = {
+            const selectedMovieWithId = {
                 ...newMovie,
                 id: Date.now(),
             };
 
-            setAllMovies((prev) => [newMovieWithId, ...prev]);
+            setAllMovies((prev) => [selectedMovieWithId, ...prev]);
             setNewMovie({});
             setNewRating(0);
             setSelectedTitle(null);
             setSelectedRating(null);
-        } else {
-            alert("Please add a movie and rate it");
         }
     };
 
+    const handleRatingChange = (movieId, newRating) => {
+        const updatedMovies = allMovies.map((movie) =>
+            movie.id === movieId ? { ...movie, rating: newRating } : movie
+        );
+        setAllMovies(updatedMovies);
+    };
+
     const handleDelete = (movieIdToRemove) => {
-        setAllMovies((prev) => prev.filter(
-            (movie) => movie.id !== movieIdToRemove
-        ));
+        setAllMovies((prev) =>
+            prev.filter((movie) => movie.id !== movieIdToRemove)
+        );
     };
 
     const sendProfile = async (url) => {
         const data = JSON.stringify(allMovies);
         try {
             const response = await fetch(url, {
-                method: 'POST',
+                method: "POST",
                 body: data,
                 headers: {
-                    'Content-type': 'application/json',
+                    "Content-type": "application/json",
                 },
             });
-            if(response.ok) {
+            if (response.ok) {
                 const jsonResponse = await response.json();
-                    setUserData(jsonResponse.userData);
-                    setItemData(jsonResponse.itemData);
-                    setUserMovies(JSON.parse(jsonResponse.userMovies));
-                    setMovieUsers(JSON.parse(jsonResponse.movieUsers));
-                    setRecommendedItems(jsonResponse.recommendedItems);
-                    setIsLoadingData(false);
-                };
+                setUserData(jsonResponse.userData);
+                setItemData(jsonResponse.itemData);
+                setUserMovies(JSON.parse(jsonResponse.userMovies));
+                setMovieUsers(JSON.parse(jsonResponse.movieUsers));
+                setRecommendedItems(jsonResponse.recommendedItems);
+                setIsLoadingData(false);
+            }
         } catch (error) {
             console.log(error);
             setIsLoadingData(false);
-        };
+        }
     };
 
     const handleSubmit = (event) => {
-        if (allMovies.length == 0){
-            alert("Add movies to generate recommendations.");
+        if (allMovies.length === 0) {
+            alert("Add movies to your profile to generate recommendations.");
         } else {
             event.preventDefault();
             const url = "http://localhost:5000/data";
@@ -130,20 +168,23 @@ export const UserProfile = () => {
             sendProfile(url);
         }
     };
+
     return (
         <>
-            <div className='UserProfile' style={{top: isGeneratingRecs ? '250px' : '30px'}} >
-                <h1 id="userprofile">Create a Movie Profile</h1>
+            <div
+                className="UserProfile"
+                style={{ top: isGeneratingRecs ? "250px" : "30px" }}
+            >
+                <h1 id="user-profile-header">Create a Movie Profile</h1>
                 <NewMovie
                     newMovie={newMovie}
                     setNewMovie={setNewMovie}
                     newRating={newRating}
-                    handleAddMovie={handleAddMovie}
                     movieTitles={movieTitles}
                     selectedTitle={selectedTitle}
                     selectedRating={selectedRating}
-                    handleTitleChange={handleTitleChange}
-                    handleRatingChange={handleRatingChange}
+                    handleTitleSelect={handleTitleSelect}
+                    handleRatingSelect={handleRatingSelect}
                 />
                 <div id='buttonGroup' className="d-flex justify-content-between align-items-center mb-4">
                     <Button id='addMovieButton' variant="primary" onClick={handleAddMovie} size="sm">
@@ -153,7 +194,11 @@ export const UserProfile = () => {
                         Generate Recommendations
                     </Button>
                 </div>
-                <MovieList allMovies={allMovies} handleDelete={handleDelete} />
+                <MovieList
+                    allMovies={allMovies}
+                    handleDelete={handleDelete}
+                    handleRatingChange={handleRatingChange}
+                />
             </div>
 
             <InitialPage
@@ -161,21 +206,30 @@ export const UserProfile = () => {
                 isGeneratingRecs={isGeneratingRecs}
             />
             {isLoadingPosters && (
-            <div className='loading-background'>
-            <p id='loading-text'>Loading Data ...</p>
-            </div>
+                <div className="loading-background">
+                    <p id="loading-text">Loading Data ...</p>
+                </div>
             )}
             {!isLoadingData && (
-                <Figures
-                    initUserData={userData}
-                    initItemData={itemData}
-                    userMovies={userMovies}
-                    movieUsers={movieUsers}
-                    recommendedItems={recommendedItems}
-                    isGeneratingRecs={isGeneratingRecs}
-                    setIsLoadingPosters={setIsLoadingPosters}
-                />
-            )};
+                <>
+                    <Figures
+                        initUserData={userData}
+                        initItemData={itemData}
+                        userMovies={userMovies}
+                        movieUsers={movieUsers}
+                        recommendedItems={recommendedItems}
+                        isGeneratingRecs={isGeneratingRecs}
+                        setIsLoadingPosters={setIsLoadingPosters}
+                    />
+                    <Steps
+                            enabled={showSteps}
+                            steps={recsSteps}
+                            initialStep={0}
+                            onExit={() => setRecsStepsEnabled(false)}
+                            onComplete={() => setShowSteps(false)}
+                    />
+                </>
+            )}
         </>
     );
 };
