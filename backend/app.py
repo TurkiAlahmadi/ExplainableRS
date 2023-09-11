1b28d29cbf352c96b651d9f207f5edfecaffa620
Turki Alahmadi <turki.369@gmail.com>
2023-09-11 02:24:35 +0200

    remove CORS settings

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from MFExplainer import MFExplainer
 app = Flask(__name__)
-CORS(app, origins='http://localhost:3000')
 
 @app.route("/data", methods= ["GET", "POST"])
 def post_and_get_data():
